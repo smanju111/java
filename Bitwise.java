79ebba069a773db26764755658f3f77c97319d2d
Smanju <122190370+smanju111@users.noreply.github.com>
2024-02-01 10:59:45 +0530

    Add files via upload

---
+++ Bitwise.java
@@ -0,0 +1,28 @@
+//Bitwise operators
+class Bit
+{
+public static void main(String args[]){
+int a=30, b=20, c;
+System.out.println("a="+a);
+System.out.println("b="+b);
+
+int addition = a+b;
+int subtract = a-b;
+int product = a*b;
+int division = a/b;
+int modulus = a%b;
+int or=a|b;
+int xor = a^b;
+int lshift=a<<2;
+int rshift=b>>1;
+System.out.println("add="+addition);
+System.out.println("sub="+subtract);
+System.out.println("prod="+product);
+System.out.println("div="+division);
+System.out.println("mod="+modulus);
+System.out.println("or="+or);
+System.out.println("xor="+xor);
+System.out.println("l="+lshift);
+System.out.println("r="+rshift);
+}
+}
